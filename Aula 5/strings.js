3c2118f9162a85d3d619d39d5f4adbc694fde7e8
Kolling <kollingleti@gmail.com>
2025-07-22 08:39:48 -0300

    aula 5 - leticia

---
+++ Aula 5/strings.js
@@ -0,0 +1,8 @@
+let umaString = "um texto"
+umaString[3]
+umaString.charAt(4)
+umaString.concat(" um", "texto ", "")
+umaString.replace("um", "outra")
+umaString. 
+
+console.log(umaString.replace("um", "outra"));
---
+++ Aula 5/stringsExemplo.js
@@ -0,0 +1,14 @@
+var a = "Javascript é incrível"
+
+console.log(a[4])
+console.log(a.charAt(4))
+console.log(a.replace("incrível", "poderoso"))
+console.log(a.length)
+console.log(a.slice(0,10))
+console.log(a.concat(" Vamos aprender!"))
+
+
+
+git commit -m "aula 5-leticia"
+git commit --allow-empty
+git commit -a -a ""
